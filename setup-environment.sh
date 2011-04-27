e0fb76076fc0a610dc5cfa4a787440de4044e1c4
Sebastian Kurfuerst <sebastian@typo3.org>
2011-04-27 15:58:55 +0200

    Adding CSS file

---
+++ setup-environment.sh
@@ -0,0 +1,8 @@
+apt-get install xsltproc
+
+
+apt-get install php-pear
+pear channel-discover pear.phing.info
+pear install phing/phing
+pear install channel://pear.php.net/Net_URL2-0.3.1
+pear install channel://pear.php.net/HTTP_Request2-2.0.0beta3
